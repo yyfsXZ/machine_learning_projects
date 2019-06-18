9c5b46f87afc6a0b7597c9f5feb9cc797b84e5a6
张翔 <zhangxiang8@xiaomi.com>
2019-06-18 15:17:05 +0800

    update es-search lib

--- CommonLibs/database/EsSearch.py
+++ CommonLibs/database/EsSearch.py
@@ -18,6 +18,25 @@ class EsSearcher:
             print "failed to connect to es, err=%s" % err
             return False
 
+    def get_index_count(self, index_name):
+        return self.es.count(index=index_name)["count"]
+
+    def get_index_querys(self, index_name):
+        query_json = {"match_all": {}}
+        # res = self.es.search(index=index_name, body={"query": query_json})
+        # return res["hits"]["hits"]
+        query = es.search(index=index_name, body={"query": query_json}, scroll='5m', size=100)
+
+        results = query['hits']['hits'] # es查询出的结果第一页
+        total = query['hits']['total']  # es查询出的结果总量
+        scroll_id = query['_scroll_id'] # 游标用于输出es查询出的所有结果
+
+        for i in range(0, int(total/100)+1):
+            # scroll参数必须指定否则会报错
+            query_scroll = es.scroll(scroll_id=scroll_id, scroll='5m')['hits']['hits']
+            results += query_scroll
+        return results
+
     def search_by_query(self, index_name, query, max_return_num=10):
         query_json = {"match": {"question": {"query": query, "type": "boolean"}}}
         res = self.es.search(index=index_name, body={"query": query_json}, size=max_return_num)
